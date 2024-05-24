0913a584d38591f72414ab6954f9ae26e571455b
ekrispy <estevan.crispin@gmail.com>
2024-05-23 22:17:57 -0600

    Fixed the key on AnimeItem

--- src/Components/AnimeList.jsx
+++ src/Components/AnimeList.jsx
@@ -6,11 +6,11 @@ const AnimeList = ({ animes }) => (
   
   <div id="results">
     {
-      // Use the map() function to iterate over the array of anime objects
+      // Use the map() function to iterate over the array of anime 
       animes.map((anime) => (
         // Render an AnimeItem component for each anime object, passing the anime object as a prop
-        // Use the mal.id property of the anime object as the key for the component
-        <AnimeItem key={anime.mal.id} anime={anime} />
+        // Use the mal_id property of the anime object as the key for the component
+        <AnimeItem key={anime.mal_id} anime={anime} />
       ))
     }
   </div>
